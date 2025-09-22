4ebd29b3ed73eaedd94f4f80e274ba523fe32bbd
unre4lbg <unre4lbg@gmail.com>
2025-09-22 07:42:38 +0300

    Windows XP дизайн - само CSS промени

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        'win98-gray': '#C0C0C0',
-        'win98-light-gray': '#DFDFDF',
+        'win98-gray': '#D4D0C8',
+        'win98-light-gray': '#F0F0F0',
         'win98-dark-gray': '#808080',
         'win98-blue': '#0A246A',
         'win98-light-blue': '#A6CAF0',
-        'win98-desktop': '#008080',
-        'icq-green': '#008000',
-        'icq-blue': '#0000FF',
-        'icq-orange': '#FF8000',
-        'icq-msg-own': '#E6F3FF',
-        'icq-msg-other': '#F0F8E6',
-        'chat-message-bg': '#FFFEF7',
-        'chat-bot-text': '#008000',
-        'chat-moderator-text': '#0000FF',
-        'chat-border': '#E0E0E0',
+        'win98-desktop': '#245EDC',
       },
       fontFamily: {
-        'win98': ['Tahoma', 'MS Sans Serif', 'Arial', 'sans-serif'],
+        'win98': ['Tahoma', 'MS Sans Serif', 'sans-serif'],
       },
     },
   },
